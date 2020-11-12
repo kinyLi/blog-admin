d6acce5447eb603fcf5e966a5e4119d9b3f26a81
lijinyi_vendor <lijinyi_vendor@sensetime.com>
2020-11-12 14:07:13 +0800

    feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.scss';
 import Login from './view/login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Link to="login">1111</Link>
+        <Route path="/login" component={Login}/>
+      </Router>
     </div>
   );
 }
