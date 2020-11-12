8db0f6ebad356a03e88676e2950a206a7f53beba
lijinyi_vendor <lijinyi_vendor@sensetime.com>
2020-11-12 14:28:44 +0800

    feat: init antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import 'antd/dist/antd.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- src/view/login/index.tsx
+++ src/view/login/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 function Login() {
   return (
-    <div>logins</div>
+    <div>
+    </div>
   )
 }
 
