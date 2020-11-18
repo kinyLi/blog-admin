8342a69b9b0deaf0d0e867588bd6f4e79e52e34a
lijinyi_vendor <lijinyi_vendor@sensetime.com>
2020-11-18 17:29:40 +0800

    style: login style

--- src/view/login/components/register-modal/index.tsx
+++ src/view/login/components/register-modal/index.tsx
@@ -1,14 +1,66 @@
-import React from 'react';
-import { Modal, Button } from 'antd';
+import React, { useState } from 'react';
+import { Modal, Button, Form, Input } from 'antd';
+import { AntdLayoutInterface } from 'utils/interface/antd';
 import { RegisterModalPropsInterface } from './index.interface';
 
+import './index.scss';
+
+const inputLayout: AntdLayoutInterface = {
+    labelCol: { span: 24 },
+    wrapperCol: { span: 24 },
+};
+
 const RegisterModal = (props: RegisterModalPropsInterface) => {
+    const [loading, setLoading] = useState(false);
     const { visible, changeIsShowRegister } = props;
+
+    const changeLodingState = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    };
 
     return (
         <div className="register-modal-view">
-            <Modal visible={visible} onCancel={changeIsShowRegister}>
-                register
+            <Modal
+                visible={visible}
+                keyboard
+                onCancel={changeIsShowRegister}
+                footer={[
+                    <Button key="close" onClick={changeIsShowRegister}>
+                        返回
+                    </Button>,
+                    <Button key="submit" type="primary" loading={loading} onClick={changeLodingState}>
+                        注册
+                    </Button>,
+                ]}
+            >
+                <div className="register-modal-form">
+                    <Form {...inputLayout}>
+                        <Form.Item
+                            label="username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item
+                            label="repeatPassword"
+                            name="repeatPassword"
+                            rules={[{ required: true, message: 'Please input your repeatPassword!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                    </Form>
+                </div>
             </Modal>
         </div>
     );
