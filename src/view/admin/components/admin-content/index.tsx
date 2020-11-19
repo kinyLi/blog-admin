1395703d3b1802a06550c130542a0409274e163a
lijinyi_vendor <lijinyi_vendor@sensetime.com>
2020-11-19 11:42:20 +0800

    feat: 上传调试

--- src/view/admin/components/admin-content/index.tsx
+++ src/view/admin/components/admin-content/index.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import './index.scss';
+import { Upload, message, Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+
+const props = {
+    name: 'file',
+    action: 'http://localhost:5000/Setting/setting',
+    headers: {
+        authorization: 'authorization-text',
+    },
+    onChange(info: any) {
+        if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
+    },
+};
 
 const AdminContent = () => {
-    return <div>adminContent</div>;
+    return (
+        <div>
+            <Upload {...props}>
+                <Button icon={<UploadOutlined />}>Click to Upload</Button>
+            </Upload>
+        </div>
+    );
 };
 
 export default AdminContent;
