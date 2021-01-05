c243e90cbfe81ce32633f5adf022dbadaee6b61c
lijinyi_vendor <lijinyi_vendor@sensetime.com>
2021-01-05 09:35:55 +0800

    feat: highlight高亮代码块

--- src/components/md-edit/index.tsx
+++ src/components/md-edit/index.tsx
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 import MarkdownIt from 'markdown-it';
 import MdEditor from 'react-markdown-editor-lite';
+import HighLight from 'highlight.js';
 
 import 'react-markdown-editor-lite/lib/index.css';
+import 'highlight.js/styles/a11y-light.css';
 
-const mdParser = new MarkdownIt(/* Markdown-it options */);
+const mdParser = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+    langPrefix: 'language-',
+    highlight(str, lang) {
+        // highlight 高亮代码块
+        if (lang && HighLight.getLanguage(lang)) {
+            try {
+                return HighLight.highlight(lang, str).value;
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        return ''; // 使用额外的默认转义
+    },
+});
 
 interface Data {
     html: string;
